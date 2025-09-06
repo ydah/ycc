50ed3f82795779d99f99e357d0d82762bf6199b7
ydah <t.yudai92@gmail.com>
2025-09-06 11:07:40 +0900

    Add support for arithmetic operations

--- ycc.c
+++ ycc.c
@@ -11,13 +11,29 @@ typedef enum {
   TOKEN_EOF,      // End of file
 } TokenKind;
 
+typedef enum {
+  NODE_ADD, // +
+  NODE_SUB, // -
+  NODE_MUL, // *
+  NODE_DIV, // /
+  NODE_NUM, // Number
+} NodeKind;
+
 typedef struct Token Token;
+typedef struct Node Node;
 
 struct Token {
   TokenKind kind; // Token type
   Token *next;    // Next token
   int val;        // If kind is TOKEN_NUM, its value
   char *str;      // Token string
+};
+
+struct Node {
+  NodeKind kind; // Node type
+  Node* lhs;     // Left hand side
+  Node* rhs;     // right hand side
+  int val;       // Use only kind is NODE_NUM
 };
 
 Token *token; // Current token
@@ -87,7 +103,7 @@ Token *tokenize() {
       continue;
     }
 
-    if (*p == '+' || *p == '-') {
+    if (strchr("+-*/()", *p)) {
       cur = new_token(TOKEN_RESERVED, cur, p++);
       continue;
     }
@@ -105,6 +121,100 @@ Token *tokenize() {
   return head.next;
 }
 
+Node* new_node(NodeKind kind) {
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  return node;
+}
+
+Node* new_binary(NodeKind kind, Node* lhs, Node* rhs) {
+  Node* node = new_node(kind);
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node* new_node_num(int val) {
+  Node* node = new_node(NODE_NUM);
+  node->val = val;
+  return node;
+}
+
+Node *expr();
+Node *mul();
+Node *primary();
+
+
+Node* expr() {
+  Node* node = mul();
+
+  for(;;) {
+    if (consume('+')) {
+      node = new_binary(NODE_ADD, node, mul());
+    } else if (consume('-')) {
+      node = new_binary(NODE_SUB, node, mul());
+    } else {
+      return node;
+    }
+  }
+}
+
+Node* mul() {
+  Node* node = primary();
+
+  for(;;) {
+    if (consume('*')) {
+      node = new_binary(NODE_MUL, node, primary());
+    } else if (consume('/')) {
+      node = new_binary(NODE_DIV, node, primary());
+    } else {
+      return node;
+    }
+  }
+}
+
+Node* primary() {
+  if (consume('(')) {
+    Node* node = expr();
+    expect(')');
+    return node;
+  }
+
+  return new_node_num(expect_number());
+}
+
+void gen(Node* node) {
+  if (node->kind == NODE_NUM) {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind) {
+  case NODE_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case NODE_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  case NODE_MUL:
+    printf("  imul rax, rdi\n");
+    break;
+  case NODE_DIV:
+    printf("  cqo\n");
+    printf("  idiv rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
+}
+
+
 int main(int argc, char **argv) {
   if (argc != 2) {
     fprintf(stderr, "Invalid number of arguments\n");
@@ -113,23 +223,15 @@ int main(int argc, char **argv) {
 
   user_input = argv[1];
   token = tokenize();
+  Node* node = expr();
 
   printf(".intel_syntax noprefix\n");
   printf(".globl main\n");
   printf("main:\n");
 
-  printf("  mov rax, %d\n", expect_number());
+  gen(node);
 
-  while (!at_eof()) {
-    if (consume('+')) {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
